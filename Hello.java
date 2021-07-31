8bad18e478edbfe8fd540b735ac611ffc9cf8ec7
shakedbo <shakedbozi@gmail.com>
2021-07-31 21:52:56 +0300

    Hello.java was added after change 4

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		for(int i=1; i<11; i++){
-			System.out.println("Hello Shaked, this is your java world project..."+i);
+		for(int i=1; i<2; i++){
+			System.out.println("I succeeded in the task and really enjoyed it..."+i);
 		}
 	}
 }
